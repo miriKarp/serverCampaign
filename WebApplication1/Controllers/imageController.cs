fd617090e50c68c2efafc98f42ea4874cbf5623d
miriKarp <m3136879@gmail.com>
2024-03-10 02:59:01 +0200

    almost finish

---
+++ WebApplication1/Controllers/imageController.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+using Server.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+
+        // private readonly string UploadFolder = Path.Combine(Directory.GetCurrentDirectory(), "Uploads"); 
+        // private static string parentDirectory = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory); // Get parent directory of application
+        private static string parentDirectory = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory()).FullName).ToString(); // Get parent directory of application
+
+        private static string relativePath = "clientCampaign/src/assets/Uploads"; // Modify this to your desired relative path
+        // string uploadFolder = Path.Combine(Directory.GetCurrentDirectory(), relativePath);
+        private readonly string UploadFolder = Path.Combine(parentDirectory, relativePath);
+
+        public ImageController(IHttpContextAccessor httpContextAccessor)
+        {
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("File is empty");
+            }
+
+            if (!Directory.Exists(UploadFolder))
+            {
+                Directory.CreateDirectory(UploadFolder);
+            }
+
+            var filePath = Path.Combine(UploadFolder, file.FileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok(new { message = "File uploaded successfully", fileName = file.FileName });
+        }
+
+        [HttpGet("files")]
+        public IActionResult GetFiles()
+        {
+            if (!Directory.Exists(UploadFolder))
+            {
+                return NotFound("Upload directory not found");
+            }
+
+            var files = Directory.GetFiles(UploadFolder);
+            var fileNames = files.Select(file => Path.GetFileName(file)).ToList();
+            System.Console.WriteLine(fileNames);
+            return Ok(fileNames);
+        }
+
+
+        // [HttpDelete("{sum}")]
+        // public Sum Delete(double sum)
+        // {
+        //     sumService.Delete(sum);
+        //     return s;
+        // }
+    }
+}
